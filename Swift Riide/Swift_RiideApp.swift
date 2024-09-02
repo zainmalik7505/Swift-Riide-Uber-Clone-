41589b377ae635d13f66c1a496f325bef0d3c852
Zain Malik <xaineil121@gmail.com>
2024-09-02 14:41:03 +0500

    Initial Commit

---
+++ Swift Riide/Swift_RiideApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_RiideApp.swift
+//  Swift Riide
+//
+//  Created by Zain Malik on 02/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Swift_RiideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
